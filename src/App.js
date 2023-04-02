5ca20685eff098f56beb70208efc767f1fc1eded
Uzair <uzairoomer@live.com>
2023-04-02 18:42:35 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>React Tutorial</h1>
+          <h1>Feedback App</h1>
         </p>
         <a
           className="App-link"
