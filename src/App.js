93d455afa620370b6d7442dc4b21f81eaac6a7ee
Uzair <uzairoomer@live.com>
2023-04-02 19:00:29 +0100

    Replace JSX in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Feedback App</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Feedback App</h1>
+      <p>Hello</p>
     </div>
   );
 }
