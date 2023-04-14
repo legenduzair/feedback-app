9af424df36d6b6825cbe9bcc1aa454a7d992f625
Uzair <uzairoomer@live.com>
2023-04-14 20:06:03 +0100

    Add aboutpage and routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { v4 as uuidv4} from 'uuid'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutPage from './pages/AboutPage'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -21,16 +23,21 @@ function App() {
   }
 
   return (
-    <div>
-      <Header />
-        <div className="container">
-          <FeedbackForm handleAdd={addFeedback} />
-          <FeedbackStats feedback={feedback}/>
-          <FeedbackList feedback={feedback}
-          handleDelete={deleteFeedback} />
-          
-        </div>
-    </div>
+    <Router>
+      <div>
+        <Header />
+          <div className="container">
+            <Route exact path='/'>
+              <FeedbackForm handleAdd={addFeedback} />
+              <FeedbackStats feedback={feedback}/>
+              <FeedbackList feedback={feedback}
+              handleDelete={deleteFeedback} />
+            </Route>
+            
+            <Route path='/about' component={AboutPage} />
+          </div>
+       </div>
+    </Router>
   );
 }
 
