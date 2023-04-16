9a9634a8965171a5060d92867545841edec1a669
Uzair <uzairoomer@live.com>
2023-04-16 21:32:22 +0100

    Update add, update and delete feedback functions to send http requests to json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,3 @@
-import { v4 as uuidv4} from 'uuid'
 import { createContext, useState, useEffect } from "react"
 
 const FeedbackContext = createContext()
@@ -17,7 +16,7 @@ export const FeedbackProvider = ({children}) => {
 
     // Fetch feedback
     const fetchFeedback = async () => {
-        const response = await fetch("http://localhost:5000/feedback?_sort=id&_order=desc")
+        const response = await fetch("/feedback?_sort=id&_order=desc")
         const data = await response.json()
 
         setFeedback(data)
@@ -25,21 +24,40 @@ export const FeedbackProvider = ({children}) => {
     }
 
     // Delete Feedback
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if(window.confirm('Are you sure you want to delete?')) {
+          await fetch(`/feedback/${id}`, { method: 'DELETE'})
           setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 
     // Update Feedback
-    const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem } : item ))
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updItem)
+        })
+
+        const data = await response.json()
+
+        setFeedback(feedback.map((item) => item.id === id ? { ...item, ...data } : item ))
     }
 
     // Add Feedback
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newFeedback)
+        })
+
+        const data = await response.json()
+        setFeedback([data, ...feedback])
     }
 
     // Edit Feedback
